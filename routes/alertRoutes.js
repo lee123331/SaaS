e858b7582ffddc2be37ef1ee6d1a46fd6f249e36
lee123331 <di702934@gmail.com>
2026-04-12 12:06:06 +0000

    Fix alert routes import

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-  getAlerts,
-  generateAlerts,
-} from "../controllers/alertController.js";
+import { getAlerts } from "../controllers/alertController.js";
 
 const router = express.Router();
 
 router.get("/", getAlerts);
-router.post("/generate", generateAlerts);
 
 export default router;
