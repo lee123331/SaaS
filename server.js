453866cff3834287ea46874ddc0aad323ea9e0a1
lee123331 <di702934@gmail.com>
2026-05-06 13:21:44 +0000

    Fix Railway CORS deployment issue

--- server.js
+++ server.js
@@ -74,7 +74,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // 모든 preflight 요청 처리
-app.options(/.*/, cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.json());
 
 const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY;
